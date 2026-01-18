d887b3e6ac1bb9ed9d023278b6b83d9ec5b3fd2f
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-01-18 17:03:02 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+mod constants;
+use crate::constants::constants::HAMLET;
+use clap::Parser;
+use std::io::{self, Write};
+
+use rand::Rng;
+use std::{thread, time};
+/// Simple program benchmark by printing hamlet
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Delay in ms on each iteration
+    #[arg(short, long, default_value_t = 0)]
+    delay: u64,
+    //// Number of times to greet
+    // #[arg(short, long, default_value_t = 1)]
+    // count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+    const ALL_CHARS: &'static str = r#"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 	
+`~!@#$%^&*()-_=+[{]}\|;:'",<.>/?"<>?"#;
+    const ALL_CHARS_INDEX_LENGTH: usize = ALL_CHARS.len() - 1;
+
+    for c in HAMLET.chars() {
+        let mut random_idx: usize = rand::rng().random_range(0..ALL_CHARS_INDEX_LENGTH);
+        let mut random_char: char = ALL_CHARS.chars().nth(random_idx).unwrap();
+        while c != random_char {
+            random_idx = rand::rng().random_range(0..ALL_CHARS_INDEX_LENGTH);
+            random_char = ALL_CHARS.chars().nth(random_idx).unwrap();
+
+            thread::sleep(time::Duration::from_millis(args.delay));
+
+            if random_char == '\t' {
+                continue;
+            }
+            if random_char == '\n' {
+                continue;
+            }
+
+            print!("{}\x08", random_char);
+            io::stdout().flush().unwrap();
+            // print!("\x1b[1D");
+        }
+        // print!("\x1b[1D");
+        // https://stackoverflow.com/questions/53162888/is-there-an-ansi-control-sequence-which-moves-the-cursor-to-the-end-of-line
+        // we need to move to end of line of previous line and remove the last character/tab(tab
+        // will be somewhat more complex) and then add new \n
+
+        print!("{}", random_char);
+        io::stdout().flush().unwrap();
+
+        // thread::sleep(time::Duration::from_millis(10));
+    }
+}
